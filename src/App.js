b315b3a603dda2e1c76f8758cc9ae04a3119fe6b
Mian-Anees <miananees23@gmail.com>
2021-08-16 12:15:50 +0500

    setup: configure pwa workers and required files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import logo from './logo.svg';
+
+<link rel="manifest" href="./manifest.json"></link>
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+import './index.css';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+<link rel="manifest" href="/manifest.json"></link>
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,7 +18,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
